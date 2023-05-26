410f5d6cb9df17059825c2c0889073dc3f5a35bd
vitalspace <myglobalsounds@gmail.com>
2023-05-26 16:49:37 -0600

    Init project

---
+++ src/App.svelte
@@ -0,0 +1,74 @@
+<script lang="ts">
+import { Project, Scene3D, PhysicsLoader, THREE } from "enable3d";
+
+class MainScene extends Scene3D {
+  box: any;
+  constructor() {
+    //@ts-ignore
+    super("MainScene");
+  }
+
+  init() {
+    console.log("Init");
+    this.renderer.setPixelRatio(1);
+    this.renderer.setSize(window.innerWidth, window.innerHeight);
+  }
+
+  preload() {
+    console.log("Preload");
+  }
+
+  create() {
+    console.log("create");
+
+    // Resize window.
+    const resize = () => {
+      const newWidth = window.innerWidth;
+      const newHeight = window.innerHeight;
+
+      this.renderer.setSize(newWidth, newHeight);
+      //@ts-ignore
+      this.camera.aspect = newWidth / newHeight;
+      this.camera.updateProjectionMatrix();
+    };
+
+    window.onresize = resize;
+    resize();
+
+    // set up scene (light, ground, grid, sky, orbitControls)
+    this.warpSpeed();
+
+    // enable physics debug
+    this.physics.debug?.enable();
+
+    // position camera
+    this.camera.position.set(10, 10, 20);
+
+    // blue box
+    this.box = this.add.box({ y: 2 }, { lambert: { color: "deepskyblue" } });
+
+    // pink box
+    this.physics.add.box({ y: 10 }, { lambert: { color: "hotpink" } });
+
+    // green sphere
+    const geometry = new THREE.SphereGeometry(0.8, 16, 16);
+    const material = new THREE.MeshLambertMaterial({ color: 0x00ff00 });
+    const cube = new THREE.Mesh(geometry, material);
+    cube.position.set(0.2, 3, 0);
+    this.scene.add(cube);
+    // add physics to an existing object
+    //@ts-ignore
+    this.physics.add.existing(cube);
+  }
+
+  update() {
+    this.box.rotation.x += 0.01;
+    this.box.rotation.y += 0.01;
+  }
+}
+
+PhysicsLoader(
+  "lib/ammo/kripken",
+  () => new Project({ scenes: [MainScene], antialias: true })
+);
+</script>
